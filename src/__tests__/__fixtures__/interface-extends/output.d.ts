dc90c2d9bde9db23cab55bccaed50295098bd005
Zihan Chen (MSFT) <zihanc@microsoft.com>
2020-11-02 04:01:17 -0800

    Cannot tell whether a flow function is a property or a method

--- src/__tests__/__fixtures__/interface-extends/output.d.ts
+++ src/__tests__/__fixtures__/interface-extends/output.d.ts
@@ -1,7 +1,6 @@
 interface IA {
   x: number
 }
-
 interface IB extends IA {
   y: number
 }
--- src/__tests__/__fixtures__/interface-members/output.d.ts
+++ src/__tests__/__fixtures__/interface-members/output.d.ts
@@ -1,21 +1,19 @@
 interface IA {
   readonly a: string
+
+  /*[FLOW2DTS - Warning] This property was a write-only property in the original Flow source.*/
   b?: null | undefined | string
 }
-
 interface IB1 {
   [x: string]: number
 }
-
 interface IB2 {
   [$: string]: number
 }
-
 interface IC {
-  c(): void
-  d(a: string, b?: null | undefined | number, ...c: number[]): boolean
+  c: () => void
+  d: (a: string, b?: null | undefined | number, ...c: number[]) => boolean
 }
-
 interface ID {
   (): void
   (a: string, b?: null | undefined | number, ...c: number[]): boolean
