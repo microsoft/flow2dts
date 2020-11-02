79c2e2211a21731bb543c96bb38bac2f10834cc2
Zihan Chen (MSFT) <zihanc@microsoft.com>
2020-11-02 03:41:35 -0800

    Format output files

--- src/__tests__/__fixtures__/interface-members/output.d.ts
+++ src/__tests__/__fixtures__/interface-members/output.d.ts
@@ -1,22 +1,22 @@
 interface IA {
-    readonly a: string;
-    b?: null | undefined | string;
+  readonly a: string
+  b?: null | undefined | string
 }
 
 interface IB1 {
-    [x: string]: number;
+  [x: string]: number
 }
 
 interface IB2 {
-    [$: string]: number;
+  [$: string]: number
 }
 
 interface IC {
-    c(): void;
-    d(a: string, b?: null | undefined | number, ...c: number[]): boolean;
+  c(): void
+  d(a: string, b?: null | undefined | number, ...c: number[]): boolean
 }
 
 interface ID {
-    (): void;
-    (a: string, b?: null | undefined | number, ...c: number[]): boolean;
+  (): void
+  (a: string, b?: null | undefined | number, ...c: number[]): boolean
 }
