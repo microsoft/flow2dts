6e678b7b0d09f5e23cb721be9d578ac133d76bfa
Eloy Durán <eloy.de.enige@gmail.com>
2020-11-05 22:45:18 +0100

    [cli] Take list of flow files to transform

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,16 @@
+import { transformFileSync } from "@babel/core"
+// @ts-ignore
+import pluginSyntaxFlow from "@babel/plugin-syntax-flow"
+import { transform as pluginFlow2DTS } from "./transform"
+
 function main() {
-  console.log("Yo")
+  const inputs = process.argv.slice(2)
+  inputs.forEach((input) => {
+    const result = transformFileSync(input, {
+      plugins: [pluginSyntaxFlow, pluginFlow2DTS],
+    })
+    console.log(result!.code)
+  })
 }
 
 main()
