c1d9401b11d7e2dfd9247122f4f97680a821801e
Eloy Durán <eloy.de.enige@gmail.com>
2020-11-09 22:47:18 +0100

    ...

--- packages/flow2dts/src/transform/__tests__/__fixtures__/type-advanced/code.js
+++ packages/flow2dts/src/transform/__tests__/__fixtures__/type-advanced/code.js
@@ -1,7 +1,7 @@
 type a = Array<number>
 type b = $ReadOnlyArray<number>
 type c = [number, () => void, boolean]
-type d = number | () => void | boolean // prettier-ignore
-type e = number & () => void & boolean // prettier-ignore
+type d = number | (() => void) | boolean
+type e = number & (() => void) & boolean
 // type f = (() => void)[]
 opaque type g = number
--- packages/flow2dts/src/transform/__tests__/__fixtures__/type-advanced/output.d.ts
+++ packages/flow2dts/src/transform/__tests__/__fixtures__/type-advanced/output.d.ts
@@ -1,9 +1,8 @@
 declare type a = number[]
 declare type b = ReadonlyArray<number>
 declare type c = [number, () => void, boolean]
-declare type d = number | (() => void) | boolean // prettier-ignore
-declare type e = number & (() => void) & boolean // prettier-ignore
-// type f = (() => void)[]
+declare type d = number | (() => void) | boolean
+declare type e = number & (() => void) & boolean // type f = (() => void)[]
 
 /*[FLOW2DTS - Warning] This type alias was opaque in the original Flow source.*/
 declare type g = number
