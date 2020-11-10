5b5bd5ea3b3cbc93d775852dd206329b2fca60f1
Eloy Durán <eloy.de.enige@gmail.com>
2020-11-10 19:34:45 +0100

    [fixupVisitor] Simplify

--- packages/flow2dts/src/transform/fixupVisitor.ts
+++ packages/flow2dts/src/transform/fixupVisitor.ts
@@ -1,27 +1,16 @@
 import { Visitor, types as t } from "@babel/core"
 import { State } from "./state"
 
-/**
- * @babel/generator's TS support does not add extra parentheses around (arrow) function types when necessary.
- */
-function ensureParenthesizedFunctionType(type: t.TSType) {
-  return t.isTSFunctionType(type) ? t.tsParenthesizedType(type) : type
-}
-
 export const fixupVisitor: Visitor<State> = {
-  TSArrayType: {
+  TSFunctionType: {
     exit(path) {
-      path.node.elementType = ensureParenthesizedFunctionType(path.node.elementType)
-    },
-  },
-  TSUnionType: {
-    exit(path) {
-      path.node.types = path.node.types.map(ensureParenthesizedFunctionType)
-    },
-  },
-  TSIntersectionType: {
-    exit(path) {
-      path.node.types = path.node.types.map(ensureParenthesizedFunctionType)
+      if (
+        path.parentPath.isTSArrayType() ||
+        path.parentPath.isTSUnionType() ||
+        path.parentPath.isTSIntersectionType()
+      ) {
+        path.replaceWith(t.tsParenthesizedType(path.node))
+      }
     },
   },
 }
