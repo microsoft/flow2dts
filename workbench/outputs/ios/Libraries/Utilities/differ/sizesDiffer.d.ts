99aab6f0600ba2bd3e511d12f000239203a7d802
Eloy Durán <eloy.de.enige@gmail.com>
2020-11-22 00:21:10 +0100

    [workbench] Add missing sizesDiffer implementation

---
+++ workbench/outputs/ios/Libraries/Utilities/differ/sizesDiffer.d.ts
@@ -0,0 +1,10 @@
+import { $TypeOf } from "flow2dts-flow-types-polyfill";
+// @flow
+// TODO: Move these Flow typings upstream
+declare type Size = {
+  width: null | undefined | number;
+  height: null | undefined | number;
+};
+declare var sizesDiffer: (one: null | undefined | Size, two: null | undefined | Size) => boolean;
+declare const $f2tExportDefault: $TypeOf<typeof sizesDiffer>;
+export default $f2tExportDefault;
