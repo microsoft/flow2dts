9fbd7dec4782780950b0729b3c0bb5402dea4591
Eloy Durán <eloy.de.enige@gmail.com>
2020-11-22 12:40:31 +0100

    [workbench] Copy missing files that were already previously added to master dump

---
+++ workbench/react-native/v0.63.3/outputs/ios/Libraries/Components/ToastAndroid/ToastAndroid.d.ts
@@ -0,0 +1,12 @@
+import { $TypeOf } from "flow2dts-flow-types-polyfill";
+
+/**
+ * TODO: Figure out why these are not included in the Flow dump
+ */
+declare var ToastAndroid: {
+  show: (message: string, duration: number) => void;
+  showWithGravity: (message: string, duration: number, gravity: number) => void;
+  showWithGravityAndOffset: (message: string, duration: number, gravity: number, xOffset: number, yOffset: number) => void;
+};
+declare const $f2tExportDefault: $TypeOf<typeof ToastAndroid>;
+export default $f2tExportDefault;
