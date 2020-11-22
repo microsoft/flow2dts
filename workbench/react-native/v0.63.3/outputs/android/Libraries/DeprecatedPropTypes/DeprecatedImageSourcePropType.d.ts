5362fb969f87ea8e8b1cf537ce46db310a9e14b0
Eloy Durán <eloy.de.enige@gmail.com>
2020-11-22 12:54:32 +0100

    [workbench] Checkin more missing files

---
+++ workbench/react-native/v0.63.3/outputs/android/Libraries/DeprecatedPropTypes/DeprecatedImageSourcePropType.d.ts
@@ -0,0 +1,18 @@
+import { $TypeOf, React$PropType$Primitive } from "flow2dts-flow-types-polyfill";
+import $3 from "prop-types";
+declare var ImageURISourcePropType: {
+  uri: $TypeOf<typeof $3.string>;
+  bundle: $TypeOf<typeof $3.string>;
+  method: $TypeOf<typeof $3.string>;
+  headers: {
+    [key: string]: string;
+  };
+  body: $TypeOf<typeof $3.string>;
+  cache: React$PropType$Primitive<"default" | "reload" | "force-cache" | "only-if-cached">;
+  width: $TypeOf<typeof $3.number>;
+  height: $TypeOf<typeof $3.number>;
+  scale: $TypeOf<typeof $3.number>;
+};
+declare var ImageSourcePropType: React$PropType$Primitive<$TypeOf<typeof ImageURISourcePropType> | $TypeOf<typeof $3.number> | $TypeOf<typeof ImageURISourcePropType>[]>;
+declare const $f2tExportDefault: $TypeOf<typeof ImageSourcePropType>;
+export default $f2tExportDefault;
