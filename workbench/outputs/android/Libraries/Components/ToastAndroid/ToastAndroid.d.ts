493ca6c0a382210fe08e17d4e1c0cc626e48602c
Eloy Durán <eloy.de.enige@gmail.com>
2020-11-22 01:08:31 +0100

    [workbench] Add missing ToastAndroid implementation

---
+++ workbench/outputs/android/Libraries/Components/ToastAndroid/ToastAndroid.d.ts
@@ -0,0 +1,19 @@
+import { $TypeOf } from "flow2dts-flow-types-polyfill";
+
+/**
+ * TODO: Figure out why these are not included in the Flow dump
+ */
+declare var ToastAndroid: {
+  // Toast duration constants
+  SHORT: number;
+  LONG: number;
+  // Toast gravity constants
+  TOP: number;
+  BOTTOM: number;
+  CENTER: number;
+  show: (message: string, duration: number) => void;
+  showWithGravity: (message: string, duration: number, gravity: number) => void;
+  showWithGravityAndOffset: (message: string, duration: number, gravity: number, xOffset: number, yOffset: number) => void;
+};
+declare const $f2tExportDefault: $TypeOf<typeof ToastAndroid>;
+export default $f2tExportDefault;
