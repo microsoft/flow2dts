34c409dc24e48fae326e27ab7a3a3a4a1a1b4bc3
Zihan Chen (MSFT) <zihanc@microsoft.com>
2020-12-04 05:37:41 -0800

    [hint] Update empty hint files

--- packages/flow2hint/src/cli.ts
+++ packages/flow2hint/src/cli.ts
@@ -18,8 +18,7 @@ async function run({
   outDir: string
   patterns: string[]
   cwd?: string
-}): Promise<[number, number]> {
-  let successCount = 0
+}): Promise<number> {
   const conversions: Array<Promise<void>> = []
   for await (const _filename of glob.stream(patterns, { absolute: true, cwd })) {
     const filename = _filename as string
@@ -32,7 +31,7 @@ async function run({
     )
   }
   await Promise.all(conversions)
-  return [conversions.length, successCount]
+  return conversions.length
 }
 
 function relativePath(cwd: string | undefined, filename: string) {
@@ -74,11 +73,9 @@ async function main() {
   const rootDir = path.resolve(cwd || "", argv.rootDir)
   const patterns = argv._
 
-  const [totalCount, successCount] = await run({ cwd, outDir, rootDir, patterns })
-
-  console.log(`\nSuccessfully converted ${successCount} of ${totalCount}\n`)
-
-  process.exit(totalCount - successCount)
+  const totalCount = await run({ cwd, outDir, rootDir, patterns })
+  console.log(`\nSuccessfully converted ${totalCount}\n`)
+  process.exit(0)
 }
 
 main()
