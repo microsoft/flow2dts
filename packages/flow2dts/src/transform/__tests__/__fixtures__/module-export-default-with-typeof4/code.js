536bf7a571ffec08171e2ae35215031ba0bc1f86
Zihan Chen (MSFT) <zihanc@microsoft.com>
2021-01-05 03:50:12 -0800

    Remove out-dated test cases

--- packages/flow2dts/src/transform/__tests__/__fixtures__/module-export-default-with-typeof4/code.js
+++
@@ -1,7 +0,0 @@
-interface Spec {}
-
-declare var NativeModule: Spec
-
-declare var NativeDeviceInfo: typeof NativeModule
-
-declare export default typeof NativeDeviceInfo
--- packages/flow2dts/src/transform/__tests__/__fixtures__/module-export-default-with-typeof4/output.d.ts
+++
@@ -1,4 +0,0 @@
-interface Spec {}
-declare var NativeModule: Spec
-declare const $f2tExportDefault: typeof NativeModule
-export default $f2tExportDefault
