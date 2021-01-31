8e49965959ed0ca575bbf8004178e7921e9eaeab
Eloy Durán <eloy.de.enige@gmail.com>
2021-01-31 11:09:26 +0100

    [transform] Do not use typeof for `import type`
    These are normal ES6 imports, but meant to remove ambiguity between exported
    values and types by the same name.

--- packages/flow2dts/src/transform/__tests__/__fixtures__/module-import/output.d.ts
+++ packages/flow2dts/src/transform/__tests__/__fixtures__/module-import/output.d.ts
@@ -3,12 +3,9 @@ import a1 from "a"
 import { b1, c1 } from "b"
 import d1, { e1, f1 } from "c"
 import * as g1 from "d"
-import a2$f2tTypeof from "e"
-declare type a2 = typeof a2$f2tTypeof
+import a2 from "e"
 import { b2 as B2, c2 as C2 } from "f"
-import d2$f2tTypeof from "g"
-declare type d2 = typeof d2$f2tTypeof
-import { e2 as E2, f2 as F2 } from "g"
+import d2, { e2 as E2, f2 as F2 } from "g"
 import a3$f2tTypeof from "h"
 declare type a3 = typeof a3$f2tTypeof
 import { b3 as b3$f2tTypeof, c3 as C3$f2tTypeof } from "i"
