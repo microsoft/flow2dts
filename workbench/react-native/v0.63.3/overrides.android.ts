383ebaa9991058f1b2348af8e75238f5bdf0bd46
Zihan Chen (MSFT) <zihanc@microsoft.com>
2021-04-07 14:50:05 -0700

    Remove ios export in android index.d.ts

---
+++ workbench/react-native/v0.63.3/overrides.android.ts
@@ -0,0 +1,65 @@
+import { OverridesVisitor } from "../../../packages/flow2dts/src/transform/applyOverridesVisitors"
+import { types as t } from "@babel/core"
+import template from "@babel/template"
+
+const ast = template({
+  plugins: ["typescript"],
+}).ast
+
+const exportsToRemove: string[] = [
+  "AccessibilityInfo",
+  "CheckBox",
+  "DatePickerIOS",
+  "Image",
+  "MaskedViewIOS",
+  "PickerIOS",
+  "ProgressViewIOS",
+  "SegmentedControlIOS",
+  "BackHandler",
+  "DatePickerAndroid",
+  "Networking",
+  "Settings",
+  "Platform",
+  "PlatformColor",
+]
+
+const visitors: OverridesVisitor[] = [
+  [
+    "index.d.ts",
+    {
+      ImportDeclaration: {
+        exit(path) {
+          if (path.node.specifiers.length === 1) {
+            const name = path.node.specifiers[0].local.name
+            if (name.endsWith("$f2tTypeof") && exportsToRemove.indexOf(name.substr(0, name.length - 10)) !== -1) {
+              path.remove()
+            }
+          }
+        },
+      },
+      ExportSpecifier: {
+        exit(path) {
+          if (path.node.exported.type === "Identifier" && exportsToRemove.indexOf(path.node.exported.name) !== -1) {
+            path.remove()
+          }
+        },
+      },
+      TSTypeAliasDeclaration: {
+        exit(path) {
+          if (exportsToRemove.indexOf(path.node.id.name) !== -1) {
+            path.remove()
+          }
+        },
+      },
+      TSPropertySignature: {
+        exit(path) {
+          if (path.node.key.type === "Identifier" && exportsToRemove.indexOf(path.node.key.name) !== -1) {
+            path.remove()
+          }
+        },
+      }
+    },
+  ],
+]
+
+export default visitors
