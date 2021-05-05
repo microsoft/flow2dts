5551a2d814c9a51f918e53556e4f9359683a80f2
Zihan Chen (MSFT) <53799235+ZihanChen-MSFT@users.noreply.github.com>
2021-05-05 15:42:41 -0700

    Delete out-dated version d8b0997a

--- workbench/react-native/d8b0997a/outputs/android/Libraries/vendor/emitter/EventEmitter.d.ts
+++
@@ -1,6 +0,0 @@
-import $1 from "./_EventEmitter";
-interface EventSubscription {
-  remove: () => void;
-}
-export default $1;
-export type { EventSubscription };
--- workbench/react-native/d8b0997a/outputs/ios/Libraries/BatchedBridge/BatchedBridge.d.ts
+++
@@ -1,4 +0,0 @@
-import $1 from "./MessageQueue";
-declare var BatchedBridge: typeof $1;
-declare const $f2tExportDefault: typeof BatchedBridge;
-export default $f2tExportDefault;
