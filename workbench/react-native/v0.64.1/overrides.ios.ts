17952398c832c257a35c64c0dfe546a9de29f451
Zihan Chen (MSFT) <53799235+ZihanChen-MSFT@users.noreply.github.com>
2021-05-24 14:45:10 -0700

    [override] Fix index.d.ts in ios import android stuff

--- workbench/react-native/v0.64.1/overrides.ios.ts
+++ workbench/react-native/v0.64.1/overrides.ios.ts
@@ -9,6 +9,62 @@ const ast = template({
 const visitors: OverridesVisitor[] = [
   // TODO: This should be fixed upstream in RN. This seems like simply broken upstream code.
   [
+    "index.d.ts",
+    {
+      ImportDeclaration: {
+        exit(path) {
+          switch (path.node.source.value) {
+            case "./Libraries/Components/DrawerAndroid/DrawerLayoutAndroid":
+            case "./Libraries/Components/ProgressBarAndroid/ProgressBarAndroid": {
+              path.remove()
+              break
+            }
+          }
+        },
+      },
+      TSTypeAliasDeclaration: {
+        exit(path) {
+          if (path.node.id.type === "Identifier") {
+            switch (path.node.id.name) {
+              case "DrawerLayoutAndroid":
+              case "ProgressBarAndroid": {
+                path.remove()
+                break
+              }
+            }
+          }
+        },
+      },
+      TSPropertySignature: {
+        exit(path) {
+          if (path.node.key.type === "Identifier") {
+            switch (path.node.key.name) {
+              case "DrawerLayoutAndroid":
+              case "ProgressBarAndroid": {
+                path.remove()
+                break
+              }
+            }
+          }
+        },
+      },
+      ExportSpecifier: {
+        exit(path) {
+          if (path.node.exported.type === "Identifier") {
+            switch (path.node.exported.name) {
+              case "DrawerLayoutAndroid":
+              case "ProgressBarAndroid": {
+                path.remove()
+                break
+              }
+            }
+          }
+        },
+      },
+    },
+  ],
+  // TODO: This should be fixed upstream in RN. This seems like simply broken upstream code.
+  [
     "Libraries/Components/SegmentedControlIOS/SegmentedControlIOS.ios.d.ts",
     {
       TSTypeReference: {
