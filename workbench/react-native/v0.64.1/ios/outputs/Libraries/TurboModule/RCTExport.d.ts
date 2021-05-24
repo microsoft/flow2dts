d8b88fb9d35602d06097f5f17b71c2be83ac1666
Zihan Chen (MSFT) <53799235+ZihanChen-MSFT@users.noreply.github.com>
2021-05-24 14:13:15 -0700

    [workbench] Initial commit for ios v0.64.1

---
+++ workbench/react-native/v0.64.1/ios/outputs/Libraries/TurboModule/RCTExport.d.ts
@@ -0,0 +1,8 @@
+// @flow
+interface DEPRECATED_RCTExport<T extends void = void> {
+  readonly getConstants?: () => {};
+}
+interface TurboModule extends DEPRECATED_RCTExport<void> {}
+export type { DEPRECATED_RCTExport };
+export type { TurboModule };
+export type { RootTag } from "../ReactNative/RootTag.js";
